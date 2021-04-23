3b281b56a2928d40dc96bf762b18caeedebeb781
Mritunjay Upadhyay <mupadhyay00@gmail.com>
2021-04-24 01:59:46 +0700

    feat: Create Auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { UsersModule } from './users/users.module';
     MongooseModule.forRoot(
       'mongodb+srv://mj:mritunjay4321@databaseone.ivhjv.gcp.mongodb.net/mj-db?retryWrites=true&w=majority',
     ),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
